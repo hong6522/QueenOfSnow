d706ddf6d79c938a8678614edbf5969a82abe359
kinbali <hgh4400@naver.com>
2023-11-10 15:56:05 +0900

    맵핑 테스트

---
+++ src/main/java/qos/controller/UsrController.java
@@ -0,0 +1,17 @@
+package qos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/usr")
+public class UsrController {
+
+	@RequestMapping("/main")
+	String main() {
+		
+		return "/usr/index";
+	}
+	
+	
+}
