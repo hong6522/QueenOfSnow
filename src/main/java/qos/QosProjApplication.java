6dc0742aa1f97329c3b0a2588a79e2820cae9bed
kinbali <hgh4400@naver.com>
2023-11-10 15:23:52 +0900

    프로젝트 생성

---
+++ src/main/java/qos/QosProjApplication.java
@@ -0,0 +1,13 @@
+package qos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QosProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QosProjApplication.class, args);
+	}
+
+}
